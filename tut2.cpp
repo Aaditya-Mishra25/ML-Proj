79a586e0aa6e26d799c9dd6a52d50237b7011e99
addy_02 <80941736+Aaditya-Mishra25@users.noreply.github.com>
2022-01-27 22:38:41 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter first number num1:\n";
+    cin >> num1;
+
+    cout << "Enter second number num2:\n";
+    cin >> num2;
+
+    cout << "The sum is" << num1 + num2;
+    return 0;
+}
